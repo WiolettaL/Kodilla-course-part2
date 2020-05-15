6ad1909c6aff337ff670467eaa749461329ad9e5
Wioletta Ledwozyw <w.ledwozyw@gmail.com_do_github>
2020-05-16 00:15:48 +0200

    get task by id

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -2,8 +2,10 @@ package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.mapper.TaskNotFoundException;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,15 +22,20 @@ public class TaskController {
     @Autowired
     private TaskMapper taskMapper;
 
+    @RequestMapping(method = RequestMethod.GET, value = "/tasks2/{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(service.getTaskById(taskId).orElseThrow(TaskNotFoundException::new));
+    }
+
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
         return taskMapper.mapToTaskDtoList(service.getAllTasks());
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "title test", "test_content");
-    }
+  //  @RequestMapping(method = RequestMethod.GET, value = "getTask")
+  //  public TaskDto getTask(Long taskId) {
+  //      return new TaskDto(1L, "title test", "test_content");
+  //  }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
     public void deleteTask(Long taskId) {
@@ -42,4 +49,5 @@ public class TaskController {
    @RequestMapping(method = RequestMethod.POST, value = "createTask")
    public void createTask(TaskDto taskDto) {
    }
+
 }

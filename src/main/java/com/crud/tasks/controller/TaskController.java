5bcbb16e9207d5dd79c71661b155d3d273feee6d
Wioletta Ledwozyw <w.ledwozyw@gmail.com_do_github>
2020-05-17 00:41:44 +0200

    delete task - task 15.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -5,12 +5,12 @@ import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.mapper.TaskNotFoundException;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
+
 
 @RestController
 @RequestMapping("/v1/task")
@@ -22,9 +22,9 @@ public class TaskController {
     @Autowired
     private TaskMapper taskMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/tasks2/{taskId}")
-    public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException {
-        return taskMapper.mapToTaskDto(service.getTaskById(taskId).orElseThrow(TaskNotFoundException::new));
+    @RequestMapping(method = RequestMethod.GET, value = "getTask")
+    public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
@@ -38,16 +38,18 @@ public class TaskController {
   //  }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteTask")
-    public void deleteTask(Long taskId) {
+    public void deleteTask(@PathVariable Long taskId) {
+        service.deleteTask(taskId);
     }
 
    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
-   public TaskDto updateTask(TaskDto taskDto) {
-       return new TaskDto(1L, "Edited test title", "Test content");
+   public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+       return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
    }
 
-   @RequestMapping(method = RequestMethod.POST, value = "createTask")
-   public void createTask(TaskDto taskDto) {
+   @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
+   public void createTask(@RequestBody TaskDto taskDto) {
+        service.saveTask(taskMapper.mapToTask(taskDto));
    }
 
 }
--- src/main/java/com/crud/tasks/mapper/TaskNotFoundException.java
+++ src/main/java/com/crud/tasks/mapper/TaskNotFoundException.java
@@ -3,6 +3,6 @@ package com.crud.tasks.mapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class TaskNotFoundException extends Exception {
 }

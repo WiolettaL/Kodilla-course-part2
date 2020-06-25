8d6cc0da0e18924e6bec4b01ee0324bb6381352f
Wioletta Ledwozyw <w.ledwozyw@gmail.com_do_github>
2020-06-25 22:39:41 +0200

    Singular or plural problem solved - Task 19.4

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -36,7 +36,7 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
-        if(!mail.getToCC().isEmpty()) {
+        if(mail.getToCC() != null) {
             mailMessage.setCc(mail.getToCC());
         }
         return mailMessage;
